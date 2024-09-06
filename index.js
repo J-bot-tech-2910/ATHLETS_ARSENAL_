8b56021e94ab3245d376725465e26b4b7b4044a6
J-bot-tech-2910 <jaynparmar29@gmail.com>
2024-09-06 10:21:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+
+function SignUpPage() {
+    window.location.href = "http://127.0.0.1:5500/sign-up-page/index.html";
+}
+function Badminton() {
+    window.location.href = "http://127.0.0.1:5500/badminton-rackets.html";
+}
+function button() {
+    window.location.href = "http://127.0.0.1:5500/Buy-now.html";
+}
+function Badminton1() {
+    window.location.href = "http://127.0.0.1:5500/badminton-rackets.html";
+}
+function grip() {
+    window.location.href = "http://127.0.0.1:5500/accessories.html";
+}
+function accessories() {
+    window.location.href = "http://127.0.0.1:5500/accessories-grip-details.html";
+}
